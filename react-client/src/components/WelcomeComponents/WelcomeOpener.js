1c09fdb1d81b8bc387180cf0a41b41a585f1091b
ForceScreamrs <barokas200@gmail.com>
2022-01-07 21:56:47 +0200

    Final touch-ups added welcome sentence

---
+++ react-client/src/components/WelcomeComponents/WelcomeOpener.js
@@ -0,0 +1,22 @@
+import { useEffect, useState, useRef } from "react";
+import React from "react";
+import './OpenerStyles.scss'
+
+export default function WelcomeOpener(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
--- react-client/src/main-pages/Welcome.js
+++ react-client/src/main-pages/Welcome.js
@@ -1,14 +1,26 @@
 import NavigationBar from '../components/NavigationBar'
+import WelcomeOpener from '../components/WelcomeComponents/WelcomeOpener'
 import '../css/pages-css/Welcome.css'
 
 function Welcome() {
     return (
         <div>
+
+            <NavigationBar />
             <div className="WelcomeContainer">
-                <NavigationBar />
-                <h1 className="Headline">Welcome!</h1>
+                <div className='HeadlineContainer'>
+                    <WelcomeOpener>
+                        <h1 className="Headline">"משפט קבלת פנים יפה כזה אבל לא משהו יותר מידי"</h1>
+                    </WelcomeOpener>
+                </div>
             </div>
+            <WelcomeOpener>
+                <div className='para'>
+                    ניסיון
+                </div>
+            </WelcomeOpener>
         </div>
+
     )
 }
 
